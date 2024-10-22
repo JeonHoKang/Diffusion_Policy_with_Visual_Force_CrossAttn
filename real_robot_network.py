fc47dbac938bd1ef1ee4ee879dc2d88059b81f41
Jeon <jeonhoka@usc.edu>
2024-10-22 13:35:12 -0700

    config

--- real_robot_network.py
+++ real_robot_network.py
@@ -139,7 +139,7 @@ class CrossAttentionFusion(nn.Module):
                 nn.Flatten()
             )
         elif im_encoder == "resnet":
-            self.image_encoder = train_utils().get_resnet("resnet18", weights= "r3m")
+            self.image_encoder = train_utils().get_resnet("resnet18", weights= None)
             self.image_encoder = train_utils().replace_bn_with_gn(self.image_encoder)
         elif im_encoder == "viT":
             self.image_encoder  = SimpleRGBObsEncoder()
